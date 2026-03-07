b75cfc7aeb7d3a8f5338adde0b2da3aac0edd29e
citybetunn212 <chentianyang021@gmail.com>
2026-03-07 22:36:36 +0800

    Update simple_harmonic_oscillator.py

--- simple_harmonic_oscillator.py
+++ simple_harmonic_oscillator.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-m = 1.0
-k = 4.0
+# 参数（你力学课学过的）
+m = 1.0      # 质量 kg
+k = 4.0      # 劲度系数 N/m
 omega = np.sqrt(k / m)
 
 t = np.linspace(0, 10, 1000)
-x = 0.5 * np.cos(omega * t)
+x = 0.5 * np.cos(omega * t)   # 初始位移 0.5m
 
 plt.plot(t, x)
 plt.title('简谐振子位移-时间图像')
